2c9c54b26d30275d11efd960cc7d6a03cfa915a0
danielmilanez <daniel.augusto.milanez@gmail.com>
2024-11-19 11:04:46 -0300

    Concluindo atividade - 19/11/2024

--- python/media_ponderada.py
+++ python/media_ponderada.py
@@ -6,4 +6,6 @@ def media():
     third = float(input('Informe a primeira nota >> ')) * 5
     return (first + second + third) / (2 + 3 + 5)
 
-print(f'A média do aluno é: {media()}')
+response = media()
+print(f'A média do aluno é: {response:.2f}', end=' ')
+print('\033[32mAPROVADO!\033[m' if response >= 5 else '\033[31mREPROVADO!\033[m')
