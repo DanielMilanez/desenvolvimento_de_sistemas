b447b6bcfcba4c17d5f81a659521e779c294c190
danielmilanez <daniel.augusto.milanez@gmail.com>
2024-11-27 10:46:32 -0300

    aprimorando jogo da forca, atualizando quantidade de temas e palavras disponiveis

--- python/atividades/jogo_da_forca/jogo_da_forca_mod.py
+++ python/atividades/jogo_da_forca/jogo_da_forca_mod.py
@@ -1,55 +1,41 @@
 from random import randint
 from os import system
-
-tema = ''
+from lista_temas import temas
 
 def sorteio(select_tema):
     system('clear')
-    frutas = ['melancia', 'pessego', 'manga', 'pera', 'morango']
-    animais = ['porco', 'cabra', 'galinha', 'vaca', 'baleia']
-    cep = ['barcelona', 'bugaria', 'argelhia', 'armenia', 'brasil']
 
-    sorteio = randint(0, 4)
+    if select_tema in temas:
+        palavra = temas[select_tema][randint(0, len(temas[select_tema]) - 1)]
+        return palavra, select_tema
+    else:
+        return False, None
 
-    match select_tema:
-        case 'frutas':
-            tema = 'frutas'
-            palavra = frutas[sorteio]
-            return palavra, tema
-        case 'animais':
-            tema = 'animais'
-            palavra = animais[sorteio]
-            return palavra, tema
-        case 'cep':
-            tema = 'cep'
-            palavra = cep[sorteio]
-            return palavra, tema
-        case _: 
-            return False
-        
+def mostrar_temas():
+    print("Temas existentes:")
+    for indice, tema in enumerate(temas.keys(), start=1):
+        print(f"{indice} - {tema.capitalize()}")
+    print('=' * 30)
+
 def game():
     print('=' * 30)
-    print('Temas existentes', end=' ')
-    print('\n - Animais \n - CEP \n - Frutas')
+    mostrar_temas()
     
     while True:
-        palavra_chave = input('Digite o tema: ').lower()
-        palavra_chave, tema = sorteio(palavra_chave)
-            
+        palavra_chave_input = input('Digite o tema: ').lower()
+        palavra_chave, tema = sorteio(palavra_chave_input)
+
         while palavra_chave == False:
             system('cls')
             print('=' * 30)
-            print('Temas existentes', end=' ')
-            print('\n - Animais \n - CEP \n - Frutas')
-            print('=' * 30)
-
-            palavra_chave = input('\033[31mTema inválido\033[m, digite um tema: ').lower().strip()
-            palavra_chave = sorteio(palavra_chave)
+            mostrar_temas()
+            palavra_chave_input = input('\033[31mTema inválido\033[m digite um tema: ').lower().strip()
+            palavra_chave, tema = sorteio(palavra_chave_input)
             
         fim = game_core(palavra_chave, tema)
         if fim == True:
             break
-            
+
 def game_core(palavra_chave, tema):
     vida = 6
     letra_digitada = set()
@@ -58,7 +44,7 @@ def game_core(palavra_chave, tema):
     
     while True:
         print(f'Você escolheu \033[32m{tema}!\033[m')
-        print('Letras digitadas >>', end=' ')
+        print('Digitados >>', end=' ')
         
         for letra in letra_digitada:
             print(letra, end=', ')
@@ -71,7 +57,10 @@ def game_core(palavra_chave, tema):
             if letra in letra_digitada:
                 print(letra, end=' ')
             else:
-                print('_', end=' ')
+                if letra == ' ':
+                    print('-', end=' ')
+                else:
+                    print('_', end=' ')
 
         tentativa = input('Seu palpite >> ').lower()
         if tentativa in letra_digitada:
@@ -82,24 +71,23 @@ def game_core(palavra_chave, tema):
             mesma_letra = False
             letra_digitada.add(tentativa)
         
-        if tentativa not in palavra_chave:
-            system('cls')
-            vida -= 1
-            print('Você \033[31mERROU\033[m! Tente novamente')
-            print(f'Você possui {vida} tentativas')
-            print('=' * 30)
-        else:
-            if mesma_letra == False:
+            if tentativa not in palavra_chave:
                 system('cls')
+                vida -= 1
+                print('Você \033[31mERROU\033[m! Tente novamente')
+                print(f'Você possui {vida} tentativas')
+                print('=' * 30)
+            else:
+                if mesma_letra == False:
+                    system('cls')
         
         if vida <= 0:
-            print(f'Você perdeu, você não conseguiu adivinhar a palavra correta, ela era {palavra_chave}')
+            print(f'Você perdeu, você não conseguiu adivinhar a palavra correta, ela era \033[31m{str(palavra_chave).upper()}\033[m')
             return True
         
         if vida == 3:
             while True:
-                select = input('Você deseja uma dica em troca de uma vida? (s/n) ')
-                
+                select = input('Você deseja uma dica em troca de uma vida? (s/n) ')                
                 match select:
                     case 's':
                         vida -= 1
@@ -113,13 +101,13 @@ def game_core(palavra_chave, tema):
 
                         system('cls')
                         print(f'Você possui {vida} tentativas')
-                        print(f'\033[34mSua dica é: {nova_letra}\033[m')
+                        print(f'\033[33mSua dica é: {nova_letra}\033[m')
                         print('=' * 30)
                         break
                     
                     case 'n':
                         break
-                
+                 
         if tentativa == palavra_chave:
             print('=' * 30)
             print(f'\033[33mVocê ganhou!\033[m Em {try_count} tentativas!')
@@ -127,4 +115,4 @@ def game_core(palavra_chave, tema):
             return True
         
         try_count += 1  
-    
+        
