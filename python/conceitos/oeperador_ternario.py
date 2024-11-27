18446a405471746054ae7b3f3ca9ab94c569f345
danielmilanez <daniel.augusto.milanez@gmail.com>
2024-11-27 10:46:58 -0300

    Estudos sobre lambda e operador ternário

---
+++ python/conceitos/oeperador_ternario.py
@@ -0,0 +1,5 @@
+# Consigo fazer uma verificação boleana em apenas uma única linha
+
+diga_ola = False
+result = print('Olá!') if diga_ola == True else print('Tchau!')
+ 
--- python/conceitos/revisao/atividade05.py
+++ python/conceitos/revisao/atividade05.py
@@ -3,13 +3,13 @@
 value = float(input('Informe o valor que você possui >> R$'))
 
 print('==' * 10, 'Conversões disponíveis', '==' * 10)
-print('\n Dólares \n Ienes \n Euros')
-opition = input('Informe qual das moedas você deseja converter >> ').strip().lower()
+print(' Dólares \n Ienes \n Euros')
 
-if opition == 'dolares':
-    print(f'O valor convertido para dol é ${(value / 5.82):.2f}')
-elif opition == 'ienes':
-    print(f'O valor convertido para Iene é ¥{(value / 0.038):.2f}')
-elif opition == 'Euros':
-    print(f'O valor convertido para Euro é €{(value / 6.06):.2f}')
+dolar = lambda value: print(f'O valor convertido para Dolares é ${(value / 5.82):.2f}')
+iene = lambda value: print(f'O valor convertido para Iene é ¥{(value / 0.038):.2f}')
+euro = lambda value: print(f'O valor convertido para Euro é €{(value / 6.06):.2f}')
+
+dolar(value) 
+iene(value)
+euro(value)
 
